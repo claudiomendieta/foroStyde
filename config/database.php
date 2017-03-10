76c14f79c4433c7f5a86860da018db4ee4f295ad
Claudio Mendieta Escumbarti <claudiomendieta@gmail.com>
2017-03-10 17:08:45 -0300

    agregando la base de datos de pruebas mysql_test

--- config/database.php
+++ config/database.php
@@ -60,7 +60,7 @@ return [
             'strict' => true,
             'engine' => null,
         ],
-        'mysql_tests' => [
+        'mysql_tests' => [ 
             'driver' => 'mysql',
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
